1de4afe1061aed23a92a62de1308b482cdffe5e2
Wendubeauty <chinwenduabia1@gmail.com>
2023-08-24 09:43:05 -0700

    line breaker

--- add.c
+++ add.c
@@ -11,7 +11,8 @@ void op_add(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack)->next || !*stack || !stack)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n",
+			line_number);
 		free_stk(stack);
 		exit(EXIT_FAILURE);
 	}
